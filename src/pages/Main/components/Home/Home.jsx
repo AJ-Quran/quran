72f0cec24b3dad568f141cca59ad37f935c14a4e
Akbar <akbarjonjorayev001@gmail.com>
2024-04-13 17:38:07 +0500

    new Subscribe page

--- src/pages/Main/components/Home/Home.jsx
+++ src/pages/Main/components/Home/Home.jsx
@@ -55,9 +55,8 @@ export default function Home({ surahI, setSurahI }) {
 
     if (btn.classList.contains('scroll_dot_btn')) {
       const index = Array.from(scrollBtns.current.children).indexOf(btn)
-      const activeDot = scrollBtns.current.querySelector('.active')
 
-      activeDot.classList.remove('active')
+      removeActiveDot()
       btn.classList.add('active')
 
       homePage.current.scrollTop = index * pageHeight
@@ -70,8 +69,7 @@ export default function Home({ surahI, setSurahI }) {
     let scrollI = homePage.current.scrollTop / pageHeight
     scrollI = Math.floor(scrollI)
 
-    const activeDot = scrollBtns.current.querySelector('.active')
-    activeDot.classList.remove('active')
+    removeActiveDot()
 
     if (direction === 'up') {
       children[scrollI].classList.add('active')
@@ -80,6 +78,18 @@ export default function Home({ surahI, setSurahI }) {
     if (direction === 'down') {
       children[scrollI + 1].classList.add('active')
     }
+  }
+
+  function scrollDotActiveI(index) {
+    removeActiveDot()
+
+    const { children } = scrollBtns.current
+    children[index].classList.add('active')
+  }
+
+  function removeActiveDot() {
+    const activeDot = scrollBtns.current.querySelector('.active')
+    activeDot.classList.remove('active')
   }
 
   async function autoFill() {
@@ -251,8 +261,8 @@ export default function Home({ surahI, setSurahI }) {
             </div>
           </div>
           <div className="txt_opa fz_mono mission_txt">
-            Our mission is to enable access to the <b>Quran</b> for all, anytime,
-            anywhere
+            Our mission is to enable access to the <b>Quran</b> for all,
+            anytime, anywhere
           </div>
           <div></div>
         </div>
@@ -305,6 +315,61 @@ export default function Home({ surahI, setSurahI }) {
             <div></div>
           </div>
         </div>
+        <div className="h_100 list_y df_ai_ce df_jc_sb scroll_area home_page_item">
+          <div className="list_y df_ai_ce two_blur_balls">
+            <b className="main_big_text">
+              <span className="txt_gradient">Subcscribe</span>
+            </b>
+            <div className="txt_opa">
+              Do not lose hope - Follow us for more! 😊
+            </div>
+          </div>
+          <div className="list_y df_ai_ce">
+            <div className="list_x subscribe_area fz_big">
+              <a
+                href="https://github.com/AJ-Quran"
+                rel="noreferrer"
+                className="con_bg_dr github"
+              >
+                <div className="df_fd_cl df_jc_sb h_100">
+                  <i className="fa-brands fa-github fz_big"></i>
+                  <b className="df_jc_end">GitHub</b>
+                </div>
+              </a>
+              <a
+                href="https://t.me/AJ_Quran"
+                rel="noreferrer"
+                className="con_bg_dr telegram"
+              >
+                <div className="df_fd_cl df_jc_sb h_100">
+                  <i className="fa-brands fa-telegram fz_big"></i>
+                  <b className="df_jc_end">Telegram</b>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div className="facts">
+            <div
+              className="con_bg_gradient main_btn active_bg_anim active"
+              onClick={() => {
+                homePage.current.scrollTop = 0
+                scrollDotActiveI(0)
+              }}
+            >
+              <div className="con_bg_dr con_ha facts_bg df_f_ce">
+                <div className="list_x df_ai_ce">
+                  <span className="material-symbols-outlined fz_normal">
+                    arrow_upward
+                  </span>
+                  <span>Explore</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="txt_opa fz_mono all_rights_txt">
+            All rights reserved © {new Date().getFullYear()}
+          </div>
+        </div>
         <div className="scroll_btns list_y df_ai_ce_child">
           <div
             className="con_bg_df con_ha up_down_btn df_f_ce"
@@ -314,6 +379,7 @@ export default function Home({ surahI, setSurahI }) {
           </div>
           <div ref={scrollBtns} className="list_y" onClick={scrollDotBtn}>
             <div className="con_bg_df con_ha scroll_dot_btn active"></div>
+            <div className="con_bg_df con_ha scroll_dot_btn"></div>
             <div className="con_bg_df con_ha scroll_dot_btn"></div>
             <div className="con_bg_df con_ha scroll_dot_btn"></div>
             <div className="con_bg_df con_ha scroll_dot_btn"></div>
