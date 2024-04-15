7b09de6dc418ea187b1f0bee366a8934ddc031ec
Akbar <akbarjonjorayev001@gmail.com>
2024-04-15 17:59:22 +0500

    new math percent abPercent

--- src/js/math/percent.js
+++ src/js/math/percent.js
@@ -2,4 +2,8 @@ function progressPercent(value, max, min = 0) {
   return ((value - min) / (max - min)) * 100
 }
 
-export { progressPercent }
+function abPercent(a, b) {
+  return (a / b) * 100
+}
+
+export { progressPercent, abPercent }
