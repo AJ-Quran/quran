e2bdf3d3aa86e2f302eb856038958e165f50bb48
Akbar <akbarjonjorayev001@gmail.com>
2024-04-30 07:15:48 +0500

    new -> Optimize button

--- src/pages/Main/components/Settings/Settings.jsx
+++ src/pages/Main/components/Settings/Settings.jsx
@@ -1,12 +1,16 @@
 import ThemeArea from './components/ThemeArea/ThemeArea'
+import OptimizeApp from './components/OptimizeApp/OptimizeApp'
 
 export default function Settings() {
   return (
     <>
-      <div className="main_w_small mar_ce">
+      <div className="main_w_small mar_ce list_y df_ai_ce w_100_child">
         <div className="con_bg_df df_jc_sb df_ai_ce">
           <div>App theme</div>
           <ThemeArea />
+        </div>
+        <div className="df_jc_ce">
+          <OptimizeApp />
         </div>
       </div>
     </>
